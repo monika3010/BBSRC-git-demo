f5f37e6cbfec1b6211b2cb1e1d41265aa4e4cfa3
monika3010 <monika.frysz@bristol.ac.uk>
2017-04-10 10:16:33 +0100

    adding lm

--- somecode.R
+++ somecode.R
@@ -3,3 +3,7 @@ df=read.table('http://data.bris.ac.uk/datasets/swyt56qr4vaj17op9cw3sag7d/Lskyeta
               header=TRUE,sep=',')
 head(df)
 
+
+# R code
+lm.result=lm(conspiracist_avg~age,data=df)
+summary(lm.result)
